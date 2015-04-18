b0b17b764ed7ee2841b62923563c7a0cb9ffe3a8
Michał Kuźmiński <tyranek@users.noreply.github.com>
2015-04-18 21:31:38 +0200

    Update Program.cs

--- bpk-keygen/Program.cs
+++ bpk-keygen/Program.cs
@@ -5,9 +5,6 @@ namespace bpk_keygen
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
